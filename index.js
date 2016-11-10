972660f9f18f88e41a8848974d1aaa9e6c8afe2d
Jonathan Lam <jlam55555@gmail.com>
2016-11-10 17:54:42 -0500

    Created basic working model with PGSQL and Socket.io

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
+// require dependencies
 var pg = require("pg");
 var app = require("express")();
 var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-app.get("/", function(req, res) {
-  res.send("<h1>hello world</h1>");
+// set routing
+app.set("view engine", "pug");
+app.use(require("express").static(__dirname + "/public"));
+http.listen(process.env.PORT || 6000, function() {
+  console.log("listening on " + (process.env.PORT || 6000));
 });
 
-http.listen(process.env.PORT || 5000, function() {
-  console.log("listening on " + process.env.PORT || 5000);
+// socket.io stuff
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function(socket) {
+    console.log("a user disconnected");
+  });
 });
 
+// db stuff
 app.get("/db", function(request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query("SELECT * FROM test_table", function(err, result) {
+    client.query("select * from test_table;", function(err, result) {
       done();
       if(err) {
         console.error(err);
         response.send("Error " + err);
-      } else {
-        response.render("pages/db", {results: result.rows} );
-      }
+      } else
+        response.render("db.jade", {results: result.rows} );
     });
   });
 });
